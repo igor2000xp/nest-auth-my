4dd7bcabf3c45f2eba2d8776910cfa2cff1fab07
igor2000xp <igorring@gmail.com>
2023-07-30 20:46:54 +0200

    feat: create reset module, controller, service, models etc.

--- src/reset/reset.controller.ts
+++ src/reset/reset.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ResetService } from './reset.service';
 
-@Controller('reset')
-export class ResetController {}
+@Controller()
+export class ResetController {
+  constructor(
+    private resetService: ResetService
+  ) {}
+
+  @Post('forgot')
+  async forgot(
+    @Body('email') email: string
+  ) {
+    const token = Math.random().toString().slice(2,8);
+    return await this.resetService.create({ email, token });
+  }
+}
--- src/reset/reset.module.ts
+++ src/reset/reset.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ResetService } from './reset.service';
 import { ResetController } from './reset.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ResetEntity } from './reset.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ResetEntity]),
+  ],
   providers: [ResetService],
-  controllers: [ResetController]
+  controllers: [ResetController],
 })
 export class ResetModule {}
